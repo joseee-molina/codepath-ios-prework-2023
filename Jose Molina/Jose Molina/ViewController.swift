493925e6660f80a31e0dbab10b6e736c612503c7
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2023-01-08 01:18:56 -0600

    Basic prework finished. Need to add style and persistence to the app. I hope to be able to do that soon since I've been busy with the wintermester and the holidays. Cheers

---
+++ Jose Molina/Jose Molina/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Jose Molina
+//
+//  Created by Jose Octavio on 28/12/22.
+//
+
+import UIKit
+import SafariServices
+
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        numberOfPetsLabel.text = "\(Int( sender.value ))"
+    }
+    
+    
+    @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        let introduction = "Howdy :) my name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I'm a CS major at \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dog. It is \(morePetsSwitch.isOn) that I want more pets. Next summer I'll be an SDE Intern at Amazon (AWS), Seattle. I want to learn iOS development so that I can create apps that will have a positive impact in my community. I have in mind apps related to mental health, finance and transportation. Please don't forget to follow me on github and contact me on Linkedin if you wish to know more. Also cheack out my website! Happy holidays :)"
+                
+           let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+           
+           let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+        func redirectHandler(alert: UIAlertAction!) {
+            let config = SFSafariViewController.Configuration()
+            config.entersReaderIfAvailable = true
+
+            let url = URL(string: "https://tavo-molina.github.io/my-website/")
+            let vc = SFSafariViewController(url: url!, configuration: config)
+            present(vc, animated: true)
+        }
+        
+            let websiteButton = UIAlertAction(title: "Go to my website", style: .default, handler: redirectHandler)
+           
+           alertController.addAction(action)
+            alertController.addAction(websiteButton)
+
+           
+           present(alertController, animated: true, completion: nil)
+
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

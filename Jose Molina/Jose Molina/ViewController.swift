97dabecf5ed06e66e5f5d34a466056b53fad80d2
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2023-01-11 23:18:07 -0600

    Added style

--- Jose Molina/Jose Molina/ViewController.swift
+++ Jose Molina/Jose Molina/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SafariServices
 
-class ViewController: UIViewController, SFSafariViewControllerDelegate {
+class ViewController: UIViewController, SFSafariViewControllerDelegate, ObservableObject {
     
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
@@ -22,10 +22,16 @@ class ViewController: UIViewController, SFSafariViewControllerDelegate {
         numberOfPetsLabel.text = "\(Int( sender.value ))"
     }
     
+   
+    @IBOutlet weak var GPATextField: UITextField!
     
+    
+    @IBOutlet weak var hobbiesTextField: UITextField!
+    
+    @IBOutlet weak var nextInternshipTextField: UITextField!
     @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
         let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
-        let introduction = "Howdy :) my name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I'm a CS major at \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dog. It is \(morePetsSwitch.isOn) that I want more pets. Next summer I'll be an SDE Intern at Amazon (AWS), Seattle. I want to learn iOS development so that I can create apps that will have a positive impact in my community. I have in mind apps related to mental health, finance and transportation. Please don't forget to follow me on github and contact me on Linkedin if you wish to know more. Also cheack out my website! Happy holidays :)"
+        let introduction = "Howdy :) my name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I'm a CS major at \(schoolNameTextField.text!). I am currently in my \(year!) year, my GPA is \(GPATextField.text!) and I own \(numberOfPetsLabel.text!) dog. It is \(morePetsSwitch.isOn) that I want more pets. My hobbies are: \(hobbiesTextField.text!) Next summer I'll be an \(nextInternshipTextField.text!), Seattle. I want to learn iOS development so that I can create apps that will have a positive impact in my community. I have in mind apps related to mental health, finance and transportation. Please don't forget to follow me on github and contact me on Linkedin if you wish to know more. Also cheack out my website! Happy holidays :)"
                 
            let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
            
@@ -48,6 +54,7 @@ class ViewController: UIViewController, SFSafariViewControllerDelegate {
            present(alertController, animated: true, completion: nil)
 
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
